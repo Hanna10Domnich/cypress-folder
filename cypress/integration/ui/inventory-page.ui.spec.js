a3c538862300502b478a165a5323cd60323cf32b
Hanna10Domnich <h.domnich@godeltecn.com>
2025-08-19 14:40:08 +0200

    add-context

--- cypress/integration/ui/inventory-page.ui.spec.js
+++ cypress/integration/ui/inventory-page.ui.spec.js
@@ -1,6 +1,7 @@
 import { inventoryPage } from '../../support/selectors';
 
 describe('InventoryPage: navigation and UI checks', { testIsolation: false }, () => {
+
     before(() => {
         cy.visit('/');
         cy.login('standard_user', 'secret_sauce');
@@ -8,42 +9,71 @@ describe('InventoryPage: navigation and UI checks', { testIsolation: false }, ()
     });
 
     context('InventoryPage: navigation menu', () => {
+
         it('should display the menu button', () => {
             cy.get(inventoryPage.menuButton).should('be.visible');
         });
     });
 
-    context('InventoryPage: filtering', () => {
-        it('should filter products A to Z', () => {
+    context('InventoryPage: when I as a user click on filter products A to Z', () => {
+
+        it('Sauce Labs Backpack should be on first place ', () => {
             cy.get(inventoryPage.filterDropdown).select('Name (A to Z)');
             cy.get(inventoryPage.inventoryItems).first().should('contain.text', 'Sauce Labs Backpack');
         });
-        it('should filter products Z to A', () => {
+    });
+
+    context('InventoryPage: when I as a user click on filter products Z to A', () => {
+
+        it('Test.allTheThings() T-Shirt (Red) should be on the first place', () => {
             cy.get(inventoryPage.filterDropdown).select('Name (Z to A)');
-            cy.get(inventoryPage.inventoryItems).first().should('not.contain.text', 'Sauce Labs Backpack');
+            cy.get(inventoryPage.inventoryItems).first().should('contain.text', 'Test.allTheThings() T-Shirt (Red)');
         });
-        it('should filter products by Price low to high', () => {
+    });
+
+    context('InventoryPage: when I as a user click on filter products by price low to high', () => {
+
+        it('should be the first product Sauce Labs Onesie', () => {
             cy.get(inventoryPage.filterDropdown).select('Price (low to high)');
             cy.get(inventoryPage.inventoryItems).first().should('contain.text', 'Sauce Labs Onesie');
         });
-        it('should filter products by Price high to low', () => {
+    });
+
+    context('InventoryPage: when I as a user click on filter products by price high to low', () => {
+
+        it('should be the first product Sauce Labs Fleece Jacket', () => {
             cy.get(inventoryPage.filterDropdown).select('Price (high to low)');
             cy.get(inventoryPage.inventoryItems).first().should('contain.text', 'Sauce Labs Fleece Jacket');
         });
     });
 
-    context('InventoryPage: menu links check: all items, logout', () => {
-        it('should open the menu and show all menu links', () => {
+    context('InventoryPage: when I as a user click on menu', () => {
+
+        before(() => {
             cy.get(inventoryPage.menuButton).click();
-            cy.get(inventoryPage.inventoryLink).should('be.visible');
+        });
+
+        it('should be about link visible', () => {
             cy.get(inventoryPage.aboutLink).should('be.visible');
+        });
+
+        it('should be all items link visible', () => {
+            cy.get(inventoryPage.inventoryLink).should('be.visible');
+        });
+
+        it('should be logout link visible', () => {
             cy.get(inventoryPage.logoutLink).should('be.visible');
+        });
+
+        it('should be reset app state link visible', () => {
             cy.get(inventoryPage.resetAppStateLink).should('be.visible');
         });
+
         it('should navigate to All Items (Inventory) page from menu', () => {
             cy.get(inventoryPage.inventoryLink).click();
             cy.url().should('include', '/inventory');
         });
+
         it('should logout and return to login page', () => {
             cy.get(inventoryPage.logoutLink).click();
             cy.url().should('include', '/');
